f83e657a46f6a367358449c63366ecfe0616b4e3
XYJeff23 <xyjjeff23@gmail.com>
2025-04-28 16:42:22 -0500

    Remove unused bootstrap.js file from static scripts directory
    The bootstrap.js file was deleted as it is no longer needed. This reduces unnecessary clutter and streamlines the project structure.

--- source/conf.py
+++ source/conf.py
@@ -11,7 +11,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import pydata_sphinx_theme
+import sphinx_rtd_theme
 # sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
@@ -51,7 +51,7 @@ autoapi_dirs = ['../src']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
