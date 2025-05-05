01fe91864f89d4b31cfda698822e549745308da9
XYJeff23 <xyjjeff23@gmail.com>
2025-05-05 15:04:15 -0500

    Refactor package configuration for source directory setup.
    Reordered and optimized `setup.py` to ensure accurate `packages` and `package_dir` definition. Removed the unnecessary `pythonpath` entry from `pyproject.toml` for a cleaner build configuration.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 setup(
     name="VlaX1D",
     version="0.1.1",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     author="Jianfeng Ye",
     author_email="xyjjeff23@gmail.com",
     description="A fast 1D Vlasov Poisson solver using JAX",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/XYJeff23/1D_Vlasov_JAX_Solver",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
     install_requires=[  # 指定依赖项
         "jax",
         "jaxlib",
